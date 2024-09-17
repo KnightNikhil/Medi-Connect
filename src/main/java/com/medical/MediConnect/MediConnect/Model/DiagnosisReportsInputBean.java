a57b42bfa54f40745dde7f6503bc2234333343fc
KnightNikhil <nikhil.the.developer@gmail.com>
2024-09-17 18:53:46 +0530

    Models Beans for backend

---
+++ src/main/java/com/medical/MediConnect/MediConnect/Model/DiagnosisReportsInputBean.java
@@ -0,0 +1,13 @@
+package com.medical.MediConnect.MediConnect.Model;
+
+public class DiagnosisReportsInputBean {
+    int medicalHistoryId;
+
+    public int getMedicalHistoryId() {
+        return medicalHistoryId;
+    }
+
+    public void setMedicalHistoryId(int medicalHistoryId) {
+        this.medicalHistoryId = medicalHistoryId;
+    }
+}
